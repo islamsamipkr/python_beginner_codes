9c3a959c6c40eebd85c5919b75b175ff1565c50e
Sami Islam <islam.samipkr@gmail.com>
2026-09-03 13:21:31 -0400

    Add Python script to print a house structure

---
+++ beginner/pythonhouse.py
@@ -0,0 +1,19 @@
+# Print the roof
+print("     /\\")
+print("    /  \\")
+print("   /____\\")
+
+# Print the walls
+print("   |    |")
+
+# Print the windows
+print("   | [] |")
+
+# Print the door
+print("   | __ |")
+
+# Print the bottom of the house
+print("   |____|")
+
+# Print a message
+print("Welcome home! 🏠")
