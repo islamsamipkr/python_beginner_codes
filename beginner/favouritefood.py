5c2cf4bedd1c505b9ee78ec92f6c7399851351d9
Sami Islam <islam.samipkr@gmail.com>
2026-09-03 13:20:30 -0400

    Add favouritefood.py to display favorite foods

---
+++ beginner/favouritefood.py
@@ -0,0 +1,14 @@
+# Print the title
+print("My Favorite Foods")
+
+# Print the first food
+print("1. Pizza 🍕")
+
+# Print the second food
+print("2. Burger 🍔")
+
+# Print the third food
+print("3. Ice Cream 🍦")
+
+# Print a funny message
+print("Now I'm hungry!")
