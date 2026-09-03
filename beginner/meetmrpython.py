928fd58fc24bdbdcfb3c74783a04d69e930b2cc1
Sami Islam <islam.samipkr@gmail.com>
2026-09-03 13:21:00 -0400

    Add meetmrpython.py to print a character

---
+++ beginner/meetmrpython.py
@@ -0,0 +1,17 @@
+# Print the hair
+print("  ///////")
+
+# Print the eyes
+print(" | O   O |")
+
+# Print the nose
+print(" |   ^   |")
+
+# Print the mouth
+print(" |  ___  |")
+
+# Print the chin
+print("  -------")
+
+# Give our character a name
+print("Meet Mr. Python!")
