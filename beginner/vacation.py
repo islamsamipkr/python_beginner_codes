fa9175022fc4eef4915ef39fe43508a034011cc5
Sami Islam <islam.samipkr@gmail.com>
2026-09-03 13:23:07 -0400

    Create vacation.py

---
+++ beginner/vacation.py
@@ -0,0 +1,17 @@
+# Print the vacation title
+print("=== MY DREAM VACATION ===")
+
+# Print the destination
+print("Destination: Maldives 🏝️")
+
+# Print the transportation
+print("Transportation: Airplane ✈️")
+
+# Print what you want to eat
+print("Food: Lots of seafood 🦐")
+
+# Print what you want to do
+print("Activity: Swimming 🌊")
+
+# Print a final message
+print("Python, please book my ticket! 😂")
