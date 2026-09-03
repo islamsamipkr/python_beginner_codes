4d98c51682d047cbf9639a5571fbca1e8d839df8
Sami Islam <islam.samipkr@gmail.com>
2026-09-03 13:22:20 -0400

    Create pythoncafe.py

---
+++ beginner/pythoncafe.py
@@ -0,0 +1,14 @@
+# Print the restaurant name
+print("=== PYTHON CAFE ===")
+
+# Print the food choices
+print("Burger ........ $8")
+print("Pizza ......... $12")
+print("Fries ......... $4")
+print("Ice Cream ..... $5")
+
+# Print a separator
+print("===================")
+
+# Print a funny message
+print("Warning: Looking at this menu may make you hungry! 😂")
