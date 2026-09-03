fff5f450e7d43bd60caacae3ec6f027ea6858307
Sami Islam <islam.samipkr@gmail.com>
2026-09-03 13:19:58 -0400

    Create printprogrammer.py

---
+++ beginner/printprogrammer.py
@@ -0,0 +1,11 @@
+# This line prints a greeting
+print("Hello everyone!")
+
+# This line prints your name
+print("My name is Sami.")
+
+# This line prints something you like
+print("I love learning Python!")
+
+# This prints a fun ending
+print("Let's become programmers! 🚀")
