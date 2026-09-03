9fdf87bd67166986dcadc58adcf8438bd1daed4f
Sami Islam <islam.samipkr@gmail.com>
2026-09-03 13:24:04 -0400

    Create pythonmakesarobot.py

---
+++ beginner/pythonmakesarobot.py
@@ -0,0 +1,21 @@
+# Print the robot's head
+print("   _______")
+print("  | O   O |")
+print("  |   -   |")
+print("  |_______|")
+
+# Print the robot's body
+print("     | |")
+print("  ---| |---")
+print("     | |")
+print("     |_|")
+
+# Print the robot's legs
+print("    /   \\")
+print("   /     \\")
+
+# Make the robot speak
+print("Hello human! 🤖")
+
+# Print a funny robot message
+print("Please give me Wi-Fi!")
