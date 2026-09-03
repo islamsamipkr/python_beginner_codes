86e5960f7ffded5ba5631cbd0f37c54e2f99c81b
Sami Islam <islam.samipkr@gmail.com>
2026-09-03 13:23:34 -0400

    Create programmerdailyschedule.py

---
+++ beginner/programmerdailyschedule.py
@@ -0,0 +1,20 @@
+# Print the morning activity
+print("8:00 AM  - Wake up 😴")
+
+# Print breakfast
+print("8:30 AM  - Drink coffee ☕")
+
+# Print coding time
+print("9:00 AM  - Start coding 💻")
+
+# Print what happens when something breaks
+print("10:00 AM - Find a bug 🐛")
+
+# Print the programmer's reaction
+print("10:01 AM - Panic 😱")
+
+# Print the solution
+print("10:05 AM - Google the error 😂")
+
+# Print the happy ending
+print("10:30 AM - Problem solved! 🎉")
