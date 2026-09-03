a3689216709cb6637ebd0670844e7fa7fcedc798
Sami Islam <islam.samipkr@gmail.com>
2026-09-03 13:22:01 -0400

    Add Christmas tree printing script

---
+++ beginner/christmastree.py
@@ -0,0 +1,17 @@
+# Print the top of the tree
+print("    *")
+
+# Print the first level
+print("   ***")
+
+# Print the second level
+print("  *****")
+
+# Print the third level
+print(" *******")
+
+# Print the tree trunk
+print("   |||")
+
+# Print a Christmas message
+print("Merry Christmas! 🎄")
